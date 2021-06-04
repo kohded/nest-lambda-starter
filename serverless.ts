13e55464f835d0485d87fa34ef274f1a4052f9fa
Arnold Koh <kohded@users.noreply.github.com>
2021-06-03 22:13:24 -0700

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,43 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  frameworkVersion: '2',
+  functions: {
+    lambda: {
+      events: [
+        {
+          http: {
+            cors: true,
+            method: 'ANY',
+            path: '/',
+          },
+        },
+        {
+          http: {
+            cors: true,
+            method: 'ANY',
+            path: '{proxy+}',
+          },
+        },
+      ],
+      handler: 'src/lambda.handler',
+      name: `CHANGE-FUNCTION-NAME-\${opt:stage}`,
+    },
+  },
+  plugins: ['serverless-plugin-typescript', 'serverless-plugin-optimize', 'serverless-offline'],
+  provider: {
+    apiGateway: {
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+    name: 'aws',
+    region: 'us-west-2',
+    runtime: 'nodejs14.x',
+  },
+  service: 'CHANGE-SERVICE-NAME',
+};
+
+module.exports = serverlessConfiguration;
